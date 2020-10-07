f3b7afaf3a0bdbbe53ee8ff9f07a80238399adab
Alysson 'Inalvejavel' Aldrin <alysson_cdf@hotmail.com>
2020-10-07 07:41:15 -0300

    PUT e PATCH

--- routes/accounts.js
+++ routes/accounts.js
@@ -42,4 +42,46 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+router.delete("/:id", async (req, res) => {
+    try {
+        const data = JSON.parse(await readFile(fileName));
+        data.accounts = data.accounts.filter(
+            account => account.id !== parseInt(req.params.id));
+        await writeFile(fileName, JSON.stringify(data, null, 2));
+        res.end();
+    } catch (err) {
+        res.status(400).send({ error: err.message});
+    }
+});
+
+router.put("/", async (req, res) => {
+    try {
+        let account = req.body;
+        const data = JSON.parse(await readFile(fileName));
+        const index = data.accounts.findIndex(
+            a => a.id === account.id);
+        data.accounts[index] = account;
+
+        await writeFile(fileName, JSON.stringify(data));
+        res.send(account);
+    } catch (err) {
+        res.status(400).send({ error: err.message});
+    }
+});
+
+router.patch("/updateBalance", async (req, res) => {
+    try {
+        let account = req.body;
+        const data = JSON.parse(await readFile(fileName));
+        const index = data.accounts.findIndex(
+            a => a.id === account.id);
+        data.accounts[index].balance = account.balance;
+
+        await writeFile(fileName, JSON.stringify(data));
+        res.send(data.accounts[index]);
+    } catch (err) {
+        res.status(400).send({ error: err.message});
+    }
+});
+
 export default router;
