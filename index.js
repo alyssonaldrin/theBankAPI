03334d797123217af2d8fd8fe4150aa65eae41f8
Alysson 'Inalvejavel' Aldrin <alysson_cdf@hotmail.com>
2020-10-07 09:18:48 -0300

    Tratamento de erros, gravação de logs e validação de campos.

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 import express from "express";
 import accountsRouter from "./routes/accounts.js";
-import {promises as fs} from "fs";
+import { promises as fs } from "fs";
+import winston from "winston";
 
 const { readFile, writeFile } = fs;
 
 global.fileName = "accounts.json";
 
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+global.logger = winston.createLogger({
+    level: "silly",
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: "theBankAPI.log" })
+    ],
+    format: combine(
+        label({ label: "theBankAPI" }),
+        timestamp(),
+        myFormat
+    )
+});
+
 const app = express();
 app.use(express.json());
-
 app.use("/account", accountsRouter);
-
 app.listen(3000, async () => {
-    
+
     try {
         await readFile(fileName);
-        console.log("API na porta 3000!");
+        logger.info("API na porta 3000!");
     } catch (err) {
         const initialJson = {
             nextId: 1,
             accounts: []
         };
-        await writeFile(fileName, JSON.stringify(initialJson));
-        console.log("json criado, API na porta 3000!");
-    }; 
+        try {
+            await writeFile(fileName, JSON.stringify(initialJson));
+            logger.info("json criado, API na porta 3000!");
+        } catch (err) {
+            logger.error(err);
+        }
+    };
 });
