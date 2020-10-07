1741df0e579fe24873147b5f499df7d40c4cb7b4
Alysson 'Inalvejavel' Aldrin <alysson_cdf@hotmail.com>
2020-10-07 06:30:17 -0300

    json de contas criado

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express";
 import accountsRouter from "./routes/accounts.js";
+import {promises as fs} from "fs";
+
+const { readFile, writeFile } = fs;
 
 const app = express();
 app.use(express.json());
 
 app.use("/account", accountsRouter);
 
-app.listen(3000, () => {
-    console.log("API na porta 3000!");
+app.listen(3000, async () => {
+    
+    try {
+        await readFile("accounts.json", );
+        console.log("API na porta 3000!");
+    } catch (err) {
+        const initialJson = {
+            nextId: 1,
+            accounts: []
+        };
+        await writeFile("accounts.json", JSON.stringify(initialJson));
+        console.log("API na porta 3000!");
+    }; 
 });
